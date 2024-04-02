66a7f44d8b32ef44e79e4f04a5454403b18d222e
pvy-it-tdh <phucvy107@gmail.com>
2024-04-02 14:13:23 +0700

    init file

---
+++ SQLQuery1.sql
@@ -0,0 +1,30 @@
+﻿CREATE DATABASE Student;
+Use Student;
+
+-- Tạo bảng Student
+CREATE TABLE Student
+(
+ID int Primary key,
+Name Nvarchar(50),
+Age INT,
+Address Nvarchar(50)
+);
+
+--Tạo bảng Course 
+
+CREATE TABLE Courses
+(
+ID INt Primary key,
+Name Nvarchar(50),
+Description Text,
+Teacher Nvarchar(50)
+);
+
+
+CREATE TABLE Enrollments (
+    StudentID INT,
+    CourseID INT,
+    EnrollmentDate DATE,
+    FOREIGN KEY (StudentID) REFERENCES Student(ID),
+    FOREIGN KEY (CourseID) REFERENCES Courses(ID)
+);
