515015be9ff26a157a79293fa6e1921e33e21e15
pvy-it-tdh <phucvy107@gmail.com>
2024-04-04 16:06:46 +0700

    update Where

---
+++ where.sql
@@ -0,0 +1,22 @@
+﻿USE TUTORIAL
+GO
+
+-- Lấy ra các đất nước khác nhau trong bảng Customer
+SELECT DISTINCT Country FROM [dbo].[Customer];
+-- LẤY RA SỐ LƯỢNG ĐẤT NƯỚC KHÁC NHAU TỪ BẢNG CUSTOMER
+
+SELECT COUNT(DISTINCT Country) from  [dbo].[Customer]
+
+Go
+
+-- Lấy ra tất cả các cột có Country='Mexico'
+
+SELECT * FROM [dbo].[Customer]
+WHERE Country='Mexico'
+go
+
+-- Lấy ra các cột ID=1 
+
+SELECT * FROM [dbo].[Customer]
+WHERE ID =1
+GO
